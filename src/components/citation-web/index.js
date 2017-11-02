ab2cd48726d34364d0d77fe8033f04d4c77d3578
Gerald <ngjingsiang@gmail.com>
2017-11-02 11:45:53 +0800

    Add basic nodes to web

--- src/components/citation-web/index.js
+++ src/components/citation-web/index.js
@@ -1,10 +1,50 @@
 import React from 'react'
+import {
+  InteractiveForceGraph,
+  ForceGraphNode,
+  ForceGraphLink
+} from 'react-vis-force'
 
-const CitationWeb = ({title, maxDepth}) => (
-  <div>
-    <p>{title}</p>
-    <p>{maxDepth}</p>
-  </div>
-)
+const CitationWeb = ({data}) => {
+  const stubData = {
+    "1": {
+      "title": "Portal gun and its conundrum",
+      "inCitations": ["2", "3"]
+    },
+    "2": {
+      "title": "Existence of gazorpgazorp",
+      "inCitations": ["3"]
+    },
+    "3": {
+      "title": "Alcoholism",
+      "inCitations": []
+    },
+  }
+
+  const getGraphNodes = data => {
+    const pubIds = Object.keys(data)
+    // TODO
+    // size scale
+    // color scale
+    return pubIds.map(id => (
+      <ForceGraphNode
+        key={id}
+        node={{
+          id: id,
+          radius: 5,
+          ...data[id]}}
+      />
+    ))
+  }
+
+  return (
+    <div>
+      <p>Citation Web</p>
+      <InteractiveForceGraph>
+        {getGraphNodes(stubData)}
+      </InteractiveForceGraph>
+    </div>
+  )
+}
 
 export default CitationWeb
