db979fbbdd6f15a924bf0577fb7274b1290e0e40
Gerald <ngjingsiang@gmail.com>
2017-11-02 16:05:53 +0800

    Change color to emphasis highlight difference

--- src/components/citation-web/index.js
+++ src/components/citation-web/index.js
@@ -28,6 +28,7 @@ const CitationWeb = ({
     return pubIds.map(id => (
       <ForceGraphNode
         key={id}
+        fill={'#EEEEEE'}
         node={{
           id: id,
           radius: radiusScale(data[id].inCitations.length),
