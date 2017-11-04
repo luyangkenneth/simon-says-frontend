74a3116c42802c5d8b391c54caaa4a9376bc19f9
Gerald <ngjingsiang@gmail.com>
2017-11-04 17:04:37 +0800

    Move help function outside of component function

--- src/components/citation-web/index.js
+++ src/components/citation-web/index.js
@@ -16,55 +16,6 @@ const CitationWeb = ({
   defaultSelectedNode,
   data
 }) => {
-  const getGraphNodes = data => {
-    const pubIds = Object.keys(data)
-
-    const inCitationLength = pubIds.map(id => data[id].inCitations.length)
-
-    const radiusScale = scaleLinear()
-      .domain([Math.min(...inCitationLength), Math.max(...inCitationLength)])
-      .range([10, 20])
-
-    return pubIds.map(id => (
-      <ForceGraphNode
-        key={id}
-        fill={'#777777'}
-        node={{
-          id: id,
-          radius: radiusScale(data[id].inCitations.length),
-          ...data[id]}}
-      />
-    ))
-  }
-
-  const getGraphLinks = data => {
-    return Object.keys(data)
-      .reduce((prev, curr) => (
-        prev.concat(data[curr].inCitations
-          .filter(inCitId => (inCitId in data))
-          .map(inCitId => ({
-            source: inCitId,
-            target: curr,
-            value: 1
-          }))
-          .map(link => (
-            <ForceGraphLink
-              key={`${link.source}->${link.target}`}
-              link={link}
-            />
-          ))
-        )
-      ), [])
-  }
-
-  const getDefaultNode = data => {
-    const ids = Object.keys(data)
-      .reduce((prev, curr) => (
-        prev.concat([{ id: curr }])
-      ), [])
-    return ids.length > 0 ? ids[0] : null
-  }
-
   return (
     <div>
       <p>Citation Web</p>
@@ -82,6 +33,55 @@ const CitationWeb = ({
       </InteractiveForceGraph>
     </div>
   )
+}
+
+function getGraphNodes(data) {
+  const pubIds = Object.keys(data)
+
+  const inCitationLength = pubIds.map(id => data[id].inCitations.length)
+
+  const radiusScale = scaleLinear()
+    .domain([Math.min(...inCitationLength), Math.max(...inCitationLength)])
+    .range([10, 20])
+
+  return pubIds.map(id => (
+    <ForceGraphNode
+      key={id}
+      fill={'#777777'}
+      node={{
+        id: id,
+        radius: radiusScale(data[id].inCitations.length),
+        ...data[id]}}
+    />
+  ))
+}
+
+function getGraphLinks(data) {
+  return Object.keys(data)
+    .reduce((prev, curr) => (
+      prev.concat(data[curr].inCitations
+        .filter(inCitId => (inCitId in data))
+        .map(inCitId => ({
+          source: inCitId,
+          target: curr,
+          value: 1
+        }))
+        .map(link => (
+          <ForceGraphLink
+            key={`${link.source}->${link.target}`}
+            link={link}
+          />
+        ))
+      )
+    ), [])
+}
+
+function getDefaultNode(data) {
+  const ids = Object.keys(data)
+    .reduce((prev, curr) => (
+      prev.concat([{ id: curr }])
+    ), [])
+  return ids.length > 0 ? ids[0] : null
 }
 
 export default CitationWeb
