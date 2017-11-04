814175847ac0c02b55aaeda2d38ea42134606035
Bili Xu <xubili93@gmail.com>
2017-11-04 19:09:54 +0800

    Update implementation for rank views

--- src/modules/rank.js
+++ src/modules/rank.js
@@ -1,9 +1,11 @@
 import { CALL_API } from 'redux-api-middleware'
 import {
-  buildUrl,
-  AUTHOR,
-  COUNT
+  topAuthorsByPublications,
+  topPublicationsByCitations
 } from '../apis/cir'
+
+export const AUTHORS = 'authors'
+export const PUBLICATIONS = 'publications'
 
 const FETCH_RANK_REQUEST = 'FETCH_RANK_REQUEST'
 const FETCH_RANK_SUCCESS = 'FETCH_RANK_SUCCESS'
@@ -26,7 +28,7 @@ export default (state = initialState, action) => {
     case FETCH_RANK_SUCCESS:
       return {
       ...state,
-        entities: action.payload.result,
+        entities: action.payload,
         loading: false
       }
 
@@ -51,9 +53,9 @@ export const getGraphData = (state = initialState, categoryKey) => {
   return { categories, data }
 }
 
-export const fetchRank = (resource) => ({
+export const fetchRank = (resource, venue, author) => ({
   [CALL_API]: {
-    endpoint: buildUrl(resource, [AUTHOR], COUNT),
+    endpoint: getUrlBuilder(resource)(venue, author),
     method: 'GET',
     types: [FETCH_RANK_REQUEST, FETCH_RANK_SUCCESS, FETCH_RANK_FAILURE]
   }
@@ -63,4 +65,16 @@ export const fetchRank = (resource) => ({
 function getSortedData(entities) {
   const entitiesArray = Object.keys(entities).map(id => entities[id])
   return entitiesArray.sort((a, b) => a.count < b.count)
+}
+
+function getUrlBuilder(resource) {
+  let endpoint;
+  switch (resource) {
+    case AUTHORS:
+      return topAuthorsByPublications
+    case PUBLICATIONS:
+      return topPublicationsByCitations
+    default:
+      return
+  }
 }
