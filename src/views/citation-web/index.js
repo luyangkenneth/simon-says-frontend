21dbdd469747c3f36fc0bb3c8b74998da961bf87
Gerald <ngjingsiang@gmail.com>
2017-11-04 11:43:44 +0800

    Remove unused methods

--- src/views/citation-web/index.js
+++ src/views/citation-web/index.js
@@ -94,65 +94,6 @@ class CitationWebView extends Component {
     const { resetSelectedPublication } = this.props
     resetSelectedPublication()
   }
-
-  getGraphNodes(data) {
-    const years = Object.keys(data)
-      .map(k => data[k])
-      .map(d => d.year)
-      .filter(y => !isNaN(y))
-
-    const colorScale = scaleLinear()
-      .domain([Math.min(...years), Math.max(...years)])
-      .range(['#EBEEF2', '#274C77'])
-
-    const citations = Object.keys(data)
-      .map(k => data[k])
-      .map(d => d.inCitations.length)
-
-    const radiusScale = scaleLinear()
-      .domain([Math.min(...citations), Math.max(...citations)])
-      .range([7, 20])
-
-    return Object.keys(data)
-      .filter(id => data[id].outCitations.length > 0)
-      .map(k => (
-        <ForceGraphNode
-          key={k}
-          fill={colorScale(data[k].year)}
-          node={{
-            id: k,
-            radius: radiusScale(data[k].inCitations.length),
-            ...data[k]
-          }}
-          showLabel={false}
-        />
-      ))
-  }
-
-  getGraphEdges(data) {
-    return Object.keys(data)
-      .filter(id => data[id].outCitations.length > 0)
-      .reduce((prev, curr) => (
-        prev.concat(data[curr].outCitations
-          .filter(cId => (cId in data))
-          .map(cId => {
-            // +1 to account for zero values
-            let value = data[curr].year - data[cId].year + 1
-            return { source: curr, target: cId, value }
-          })
-          .map(({ source, target, value }) => ({
-            source,
-            target,
-            value: isNaN(value) ? 1 : value
-          }))
-          .map(obj => (
-            <ForceGraphLink
-              key={`${obj.source}=>${obj.target}`}
-              link={obj}
-            />
-          )))
-    ), [])
-  }
 }
 
 export default CitationWebView
