7772679f6fece9018ad914088396ac2661eb5455
Bili Xu <xubili93@gmail.com>
2017-11-13 00:52:37 +0800

    Allow for custom formatter in API action

--- src/modules/api.js
+++ src/modules/api.js
@@ -44,15 +44,22 @@ export const createAPIReducer = (resource, defaultData, onSuccess = defaultOnSuc
   }
 }
 
+const defaultFormatter = (action, state, res) => res.json()
+
 /**
  * Returns a fetch API action.
  *
  * @param {string} resource - name of resource that we are fetching
  * @param {function} endpointBuilder - function that has signature (state),
  *  which returns a string to perform a fetch from
+ * @param {function} formatter - returns a custom payload formatter
  * @return {function} action - action compatible with redux-api-middleware
  */
-export const createAPIAction = (resource, endpointBuilder) => {
+export const createAPIAction = (
+  resource,
+  endpointBuilder,
+  formatter = defaultFormatter
+) => {
   return (dispatch, getState) => {
     const state = getState()
     dispatch({
@@ -61,7 +68,10 @@ export const createAPIAction = (resource, endpointBuilder) => {
         method: 'GET',
         types: [
           `FETCH_${resource.toUpperCase()}_REQUEST`,
-          `FETCH_${resource.toUpperCase()}_SUCCESS`,
+          {
+            type: `FETCH_${resource.toUpperCase()}_SUCCESS`,
+            payload: formatter
+          },
           `FETCH_${resource.toUpperCase()}_FAILURE`
         ]
       }
