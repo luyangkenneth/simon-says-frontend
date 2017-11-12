f463feee2e06907001df8ee550f8946444e0daef
Bili Xu <xubili93@gmail.com>
2017-11-12 21:40:11 +0800

    Create rank view reducer with the new API reducer factory

--- src/containers/rank/index.js
+++ src/containers/rank/index.js
@@ -1,14 +1,19 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Rank from '../../views/rank'
-import { fetchRank, updateFilter, getGraphData } from '../../modules/rank'
-import { getAllVenues, fetchVenues } from '../../modules/venues'
+import {
+  fetchRank,
+  updateFilter,
+  getGraphData
+} from '../../modules/rank'
+import {
+  getAllVenues,
+  fetchVenues
+} from '../../modules/venues'
 
 
 export default (resource, categoryKey, title) => {
   const mapStateToProps = state => ({
-    loading: state.rank.loading,
-    error: state.rank.error,
     categories: getGraphData(state.rank, categoryKey).categories,
     series: [getGraphData(state.rank, categoryKey).data],
     filters: state.rank.filters,
--- src/modules/rank.js
+++ src/modules/rank.js
@@ -1,51 +1,17 @@
-import { CALL_API } from 'redux-api-middleware'
 import { combineReducers } from 'redux'
 import {
   topAuthorsByPublications,
   topPublicationsByCitations
 } from '../apis/cir'
+import { createAPIReducer, createAPIAction } from './api'
 
 export const AUTHORS = 'authors'
 export const PUBLICATIONS = 'publications'
 
-const FETCH_RANK_REQUEST = 'FETCH_RANK_REQUEST'
-const FETCH_RANK_SUCCESS = 'FETCH_RANK_SUCCESS'
-const FETCH_RANK_FAILURE = 'FETCH_RANK_FAILURE'
 const UPDATE_FILTER = 'UPDATE_RANK_FILTER'
 
 // Reducers
-
-const initialState = {
-  entities: {},
-  loading: true,
-  error: false
-}
-
-const apiReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_RANK_REQUEST:
-      return {
-        ...state,
-        loading: true
-      }
-
-    case FETCH_RANK_SUCCESS:
-      return {
-      ...state,
-        entities: action.payload,
-        loading: false
-      }
-
-    case FETCH_RANK_FAILURE:
-      return {
-        ...state,
-        error: true
-      }
-
-    default:
-      return state
-  }
-}
+const api = createAPIReducer('rank', {})
 
 const initialFilters = {
   venue: undefined,
@@ -53,7 +19,7 @@ const initialFilters = {
   cohort: 10
 }
 
-const filtersReducer = (state = initialFilters, action) => {
+const filters = (state = initialFilters, action) => {
   switch (action.type) {
     case UPDATE_FILTER:
       const { payload } = action
@@ -67,50 +33,43 @@ const filtersReducer = (state = initialFilters, action) => {
 }
 
 export default combineReducers({
-  filters: filtersReducer,
-  apiReducer
+  filters,
+  api
 })
 
 // Selectors
 
-export const getGraphData = (state = initialState, categoryKey) => {
-  const { entities } = state.apiReducer
-  const sorted = getSortedData(entities)
+export const getGraphData = (state, categoryKey) => {
+  const { data } = state.api
+  const sorted = getSortedData(data)
   const categories = sorted.map(entity => entity[categoryKey])
-  const data = sorted.map(entity => entity.count)
+  const series = sorted.map(entity => entity.count)
 
-  return { categories, data }
+  return { categories, data: series }
 }
 
 // Actions
 
-export const fetchRank = (resource) => {
-  return (dispatch, getState) => {
-    const { filters } = getState().rank
-
-    dispatch({
-      [CALL_API]: {
-        endpoint: getUrlBuilder(resource)(
-          filters.venue,
-          filters.cohort,
-          filters.year
-        ),
-        method: 'GET',
-        types: [FETCH_RANK_REQUEST, FETCH_RANK_SUCCESS, FETCH_RANK_FAILURE]
-      }
-    })
-  }
-}
+export const fetchRank = resource => createAPIAction('rank', state => {
+  const { filters } = state.rank
+  return getUrlBuilder(resource)(
+    filters.venue,
+    filters.cohort,
+    filters.year
+  )
+})
 
 export const updateFilter = (key, value) => ({
   type: UPDATE_FILTER,
   payload: { key, value }
 })
 
+
 // Helpers
-function getSortedData(entities) {
-  const entitiesArray = Object.keys(entities).map(id => entities[id])
-  return entitiesArray.sort((a, b) => a.count < b.count)
+function getSortedData(data) {
+  return Object.keys(data)
+    .map(id => data[id])
+    .sort((a, b) => a.count < b.count)
 }
 
 function getUrlBuilder(resource) {
