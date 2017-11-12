dfdd70cba9c450b1403433aa9e9d56ef0dac8cc4
Bili Xu <xubili93@gmail.com>
2017-11-12 21:52:27 +0800

    Create venues reducer with the new API reducer factory

--- src/modules/venues.js
+++ src/modules/venues.js
@@ -1,53 +1,15 @@
-import { CALL_API } from 'redux-api-middleware'
+import { createAPIReducer, createAPIAction } from './api'
 import { venues } from '../apis/cir'
 
-const FETCH_VENUES_REQUEST = 'FETCH_VENUE_REQUEST'
-const FETCH_VENUES_SUCCESS = 'FETCH_VENUE_SUCCESS'
-const FETCH_VENUES_FAILURE = 'FETCH_VENUE_FAILURE'
-
-const initialState = {
-  loading: false,
-  error: false,
-  data: []
-}
+const RESOURCE = 'venues'
 
 // Reducer
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_VENUES_REQUEST:
-      return {
-        ...state,
-        loading: true
-      }
-    case FETCH_VENUES_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        data: action.payload
-      }
-    case FETCH_VENUES_FAILURE:
-      return {
-        ...state,
-        loading: false,
-        error: true
-      }
-    default:
-      return state
-  }
-}
+const reducer = createAPIReducer(RESOURCE, [])
 
 // Selectors
 export const getAllVenues = (state) => state.data
 
 // Actions
-export const fetchVenues = () => {
-  return {
-    [CALL_API]: {
-      endpoint: venues(),
-      method: 'GET',
-      types: [FETCH_VENUES_REQUEST, FETCH_VENUES_SUCCESS, FETCH_VENUES_FAILURE]
-    }
-  }
-}
+export const fetchVenues = () => createAPIAction(RESOURCE, () => venues())
 
 export default reducer
