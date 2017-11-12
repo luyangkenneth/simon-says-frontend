c52c824b1f95002273662f1eb80cd0bbd3f5d49c
Bili Xu <xubili93@gmail.com>
2017-11-13 01:07:00 +0800

    Implement fetching of publication trend

--- src/modules/comparison/index.js
+++ src/modules/comparison/index.js
@@ -1,7 +1,38 @@
 import { combineReducers } from 'redux'
 
+import { createAPIReducer, createAPIAction } from '../api'
+import { publicationsByYear } from '../../apis/cir'
+
 import setup from './setup'
 
+const RESOURCE = 'COMPARISON_TREND'
+
+// Reducer
+const reducer = createAPIReducer(RESOURCE, {}, (state, payload) => ({
+  ...state,
+  loading: false,
+  data: {
+    ...state.data,
+    [payload.author]: payload.data
+  }
+}))
+
+// Actions
+export const fetchPublicationTrend = author => {
+  return createAPIAction(
+    RESOURCE,
+    publicationsByYear(undefined, author),
+    (action, state, res) => {
+      return res.json().then(json => ({
+        author,
+        data: json
+      }))
+    }
+  )
+}
+
+
 export default combineReducers({
+  bank: reducer,
   setup
 })
