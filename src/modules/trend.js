7048f487f4512c3cf3e2759960f188a7786cb89f
Bili Xu <xubili93@gmail.com>
2017-11-12 21:46:19 +0800

    Create trend view reducer with the new API reducer factory

--- src/modules/trend.js
+++ src/modules/trend.js
@@ -1,52 +1,17 @@
-import { CALL_API } from 'redux-api-middleware'
 import { combineReducers } from 'redux'
 import {
   citationsByYear,
   publicationsByYear
 } from '../apis/cir'
+import { createAPIReducer, createAPIAction } from './api'
 
 export const CITATIONS = 'citations'
 export const PUBLICATIONS = 'publications'
 
-const FETCH_TREND_REQUEST = 'FETCH_TREND_REQUEST'
-const FETCH_TREND_SUCCESS = 'FETCH_TREND_SUCCESS'
-const FETCH_TREND_FAILURE = 'FETCH_TREND_FAILURE'
-
 const UPDATE_FILTER = 'UPDATE_TREND_FILTER'
 
 // Reducers
-
-const initialState = {
-  entities: {},
-  loading: true,
-  error: false
-}
-
-const apiReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FETCH_TREND_REQUEST:
-      return {
-        ...state,
-        loading: true
-      }
-
-    case FETCH_TREND_SUCCESS:
-      return {
-        ...state,
-        entities: action.payload,
-        loading: false
-      }
-
-    case FETCH_TREND_FAILURE:
-      return {
-        ...state,
-        error: true
-      }
-
-    default:
-      return state
-  }
-}
+const api = createAPIReducer('trend', {})
 
 const initialFilter = {
   author: undefined,
@@ -69,38 +34,29 @@ const filtersReducer = (state = initialFilter, action) => {
 
 export default combineReducers ({
   filters: filtersReducer,
-  apiReducer
+  api
 })
 
 // Selector
 
-export const getGraphData = (state = initialState, categoryKey) => {
-  const { entities } = state.apiReducer
-  const sorted = getSortedData(entities)
+export const getGraphData = (state, categoryKey) => {
+  const { data } = state.api
+  const sorted = getSortedData(data)
   const categories = sorted.map(item => item.year)
-  const data = sorted.map(item => item.count)
+  const series = sorted.map(item => item.count)
 
-  return { categories, data }
+  return { categories, data: series }
 }
 
 // Actions
 
-export const fetchTrend = (resource) => {
-  return (dispatch, getState) => {
-      const { filters } = getState().trend
-
-      dispatch({
-        [CALL_API]: {
-          endpoint: getUrlBuilder(resource)(
-            filters.venue,
-            filters.author
-          ),
-          method: 'GET',
-          types: [FETCH_TREND_REQUEST, FETCH_TREND_SUCCESS, FETCH_TREND_FAILURE]
-        }
-      })
-  }
-}
+export const fetchTrend = resource => createAPIAction('trend', state => {
+  const { filters } = state.trend
+  return getUrlBuilder(resource)(
+    filters.venue,
+    filters.author
+  )
+})
 
 export const updateFilter = (key, value) => ({
   type: UPDATE_FILTER,
